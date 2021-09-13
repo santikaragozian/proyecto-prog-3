622d32c08f4937bd472faf8deaf51b7eb001563f
Benja-Son <bsonne@udesa.edu.ar>
2021-09-13 10:22:45 -0300

    Api conectada

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,9 +8,27 @@ class Main extends Component{
         super()
         this.state = {
             tarjetas: [],
-            isLoaded: false
+            isLoaded: false,
+            datos: ''
         }
     }
+
+    componentDidMount(){
+        console.log('Funciona')
+        let  url = 'https://api.themoviedb.org/3/movie/popular?api_key=decfa5bfc3151df1ce9acb9ac606d5c4&language=en-US&page=1'
+        fetch(url)
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data);
+            this.setState({
+                movie: data.results,
+                isLoaded: true,
+                nextUrl:  data.info.next
+            })
+        })
+        .catch( error => console.error())
+    }
+
 
     
 
