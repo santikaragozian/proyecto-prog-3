360621c1e1f2811fa28ae036581f9ee162424e9d
Benja-Son <bsonne@udesa.edu.ar>
2021-09-13 10:43:30 -0300

    jdj

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,11 @@ function Header(){
         <h1>Título/ Nombre de la app</h1>
         <div className='headerDerecha'>
             <p>Ordenar ASC/ DESC</p>
-            <i class="fas fa-th"></i>
-            <i class="fas fa-align-justify"></i>
+            <i className="fas fa-th"></i>
+            <i className="fas fa-align-justify"></i>
             <form action="">
                 <input type="text" name="search" id="" placeholder="Search" /> 
-                <button type="submit"><i class="fas fa-search"></i></button>
+                <button type="submit"><i className="fas fa-search"></i></button>
             </form>
         </div>
         </div>
